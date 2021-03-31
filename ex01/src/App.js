586534aa208ad5ac02986488bad73600de2b28d0
Kemal Arifovic <gigalab@gmail.com>
2021-03-31 09:19:15 +0200

    Cookie - Local Storage - Session Storage - Set

---
+++ ex01/src/App.js
@@ -0,0 +1,21 @@
+// src/App.js
+import React, { Component } from "react";
+
+class App extends Component {
+  setMyStorage = () => {
+    document.cookie = "Year=2021";
+    localStorage.setItem("Paragon", "yes, but Arena first");
+    sessionStorage.setItem("frontend", "React");
+  };
+  render() {
+    return (
+      <div className="App">
+        <button type="button" onClick={this.setMyStorage}>
+          Button
+        </button>
+      </div>
+    );
+  }
+}
+
+export default App;
