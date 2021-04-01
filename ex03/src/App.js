3dfaea4d1ae368ef55e13c730ded2d6750caebf6
Kemal Arifovic <gigalab@gmail.com>
2021-04-01 08:07:15 +0200

    Local Storage - setItem

---
+++ ex03/src/App.js
@@ -0,0 +1,19 @@
+// src/App.js
+import React, { Component } from "react";
+
+class App extends Component {
+  setLocalStorage = () => {
+    localStorage.setItem("Arena", "Selection Month");
+  };
+  render() {
+    return (
+      <div className="App">
+        <button type="button" onClick={this.setLocalStorage}>
+          Button
+        </button>
+      </div>
+    );
+  }
+}
+
+export default App;
